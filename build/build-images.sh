33d6e274f71a2b5d7a131b3d8fea128dae17f3c8
BlackEagle <ike.devolder@gmail.com>
2019-01-04 10:08:59 +0100

    add docker build with dockerwest base
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ build/build-images.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [[ -z $1 ]]; then
+    echo "give the version to build"; exit 1;
+fi
+version=$1
+
+cd "$(dirname "$(readlink -f $0)")"
+
+[ -d application ] && rm -rf application
+
+git clone git@github.com:BlackIkeEagle/akaunting.git application
+cd application
+git checkout $version
+composer install --no-dev
+cp -aL public public-full
+rm -rf public
+mv public-full public
+rm -rf .git*
+
+cd ..
+docker build --squash=true -f Dockerfile-nginx -t blackikeeagle/akaunting-nginx:latest .
+docker build --squash=true -f Dockerfile-php -t blackikeeagle/akaunting-php:latest .
+
+docker push blackikeeagle/akaunting-nginx:latest
+docker push blackikeeagle/akaunting-php:latest
+
+rm -rf application
